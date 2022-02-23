e0b9217f77366972f65aa07b724ca422da841a4e
Alex <pioneerko@hotmail.com>
2022-02-23 20:33:16 +0000

    Setup project

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import os
 from flask import Flask
+from flask import render_template
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return render_template("index.html", index_page=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=False)
